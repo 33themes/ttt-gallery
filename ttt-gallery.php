96d719c892bc3ebd8039793e87786b8f304f98ce
gabrielperezs <gabriel@guero.net>
2014-08-26 21:47:11 +0200

    Move global variable

--- ttt-gallery.php
+++ ttt-gallery.php
@@ -17,34 +17,35 @@ define('TTTVERSION_GALLERY', 0.1 );
 
 
 function ttt_autoload_gallery( $class ) {
-    if ( 0 !== strpos( $class, 'TTTGallery_' ) )
-        return;
-    
-    $file = TTTINC_GALLERY . '/class/' . $class . '.php';
-    if (is_file($file))
-        require_once $file;
-        return true;
-    
-    throw new Exception("Unable to load $class at ".$file);
+	if ( 0 !== strpos( $class, 'TTTGallery_' ) )
+		return;
+	
+	$file = TTTINC_GALLERY . '/class/' . $class . '.php';
+	if (is_file($file))
+		require_once $file;
+		return true;
+	
+	throw new Exception("Unable to load $class at ".$file);
 }
 
 if ( function_exists( 'spl_autoload_register' ) ) {
-    spl_autoload_register( 'ttt_autoload_gallery' );
+	spl_autoload_register( 'ttt_autoload_gallery' );
 } else {
-    require_once TTTINC_GALLERy . '/class/TTTGallery_Common.php';
+	require_once TTTINC_GALLERy . '/class/TTTGallery_Common.php';
 }
 
 function tttgallery_init () {
-    $s = load_plugin_textdomain( 'tttgallery', false, dirname( plugin_basename( __FILE__ ) ) . '/lang/' );
-    if ( !is_admin() ) {
-        global $TTTGallery_Front;
-        $TTTGallery_Front = new TTTGallery_Front();
-        $TTTGallery_Front->init();
-    }
-    else {
-        $TTTGallery_Admin = new TTTGallery_Admin();
-        $TTTGallery_Admin->init();
-    }
+	$s = load_plugin_textdomain( 'tttgallery', false, dirname( plugin_basename( __FILE__ ) ) . '/lang/' );
+	if ( !is_admin() ) {
+		global $TTTGallery_Front;
+		$TTTGallery_Front = new TTTGallery_Front();
+		$TTTGallery_Front->init();
+	}
+	else {
+		global $TTTGallery_Front;
+		$TTTGallery_Admin = new TTTGallery_Admin();
+		$TTTGallery_Admin->init();
+	}
 
 }
 
@@ -54,11 +55,11 @@ add_action('init', 'tttgallery_init');
 register_uninstall_hook( __FILE__ , 'tttgallery_uninstall' );
 
 function tttgallery_uninstall() {
-    require_once TTTINC_GALLERY . '/class/TTTGallery_Common.php';
-    require_once TTTINC_GALLERY . '/class/TTTGallery_Admin.php';
+	require_once TTTINC_GALLERY . '/class/TTTGallery_Common.php';
+	require_once TTTINC_GALLERY . '/class/TTTGallery_Admin.php';
 
-    $TTTGallery_Admin = new TTTGallery_Admin();
-    $TTTGallery_Admin->uninstall();
+	$TTTGallery_Admin = new TTTGallery_Admin();
+	$TTTGallery_Admin->uninstall();
 }
 
 
