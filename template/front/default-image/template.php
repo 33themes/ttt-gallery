5dd1000151e237a4733196fef81bd6bd80765d78
Gabriel Pérez S <gabriel@guero.net>
2014-09-15 22:26:51 +0200

    Add new template folder for images

--- template/front/default-image/template.php
+++ template/front/default-image/template.php
@@ -1,4 +1,4 @@
-<div class="tttgallery">
+<div class="tttgallery oneimage">
 	<h2><?php $ttt_gallery->description; ?></h2>
 	<?php foreach( $ttt_gallery->medias as $ttt_media ): ?>
 	<a class="thickbox" href="<?php echo $ttt_media['sizes']['full']['url']; ?>" title="<?php echo $ttt_media['description']; ?>" rel="<?php echo $ttt_gallery->rel; ?>">
