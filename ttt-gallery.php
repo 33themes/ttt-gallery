a26fe1a935bd26cfc0ba2d42f704d306fe957fc3
Gabriel Pérez S <gabriel@guero.net>
2015-02-02 17:10:36 +0100

    Remove tabs and replace for 4 spaces

--- ttt-gallery.php
+++ ttt-gallery.php
@@ -17,39 +17,39 @@ define('TTTVERSION_GALLERY', 0.1 );
 
 
 function ttt_autoload_gallery( $class ) {
-	if ( strpos( $class, 'TTTGallery' ) === false )
-		return;
-	
-	$file = TTTINC_GALLERY . '/class/' . $class . '.php';
+    if ( strpos( $class, 'TTTGallery' ) === false )
+        return;
+    
+    $file = TTTINC_GALLERY . '/class/' . $class . '.php';
     if (is_file($file)) {
-		require_once $file;
-		return true;
+        require_once $file;
+        return true;
     }
-	
-	throw new Exception("Unable to load $class at ".$file);
+    
+    throw new Exception("Unable to load $class at ".$file);
 }
 
 if ( function_exists( 'spl_autoload_register' ) ) {
-	spl_autoload_register( 'ttt_autoload_gallery' );
+    spl_autoload_register( 'ttt_autoload_gallery' );
 } else {
-	require_once TTTINC_GALLERy . '/class/TTTGallery_Common.php';
-	require_once TTTINC_GALLERy . '/class/TTTGallery_Admin.php';
-	require_once TTTINC_GALLERy . '/class/TTTGallery_Front.php';
-	require_once TTTINC_GALLERy . '/class/TTTGallery.php';
+    require_once TTTINC_GALLERy . '/class/TTTGallery_Common.php';
+    require_once TTTINC_GALLERy . '/class/TTTGallery_Admin.php';
+    require_once TTTINC_GALLERy . '/class/TTTGallery_Front.php';
+    require_once TTTINC_GALLERy . '/class/TTTGallery.php';
 }
 
 function tttgallery_init () {
-	$s = load_plugin_textdomain( 'tttgallery', false, dirname( plugin_basename( __FILE__ ) ) . '/lang/' );
-	if ( !is_admin() ) {
-		global $TTTGallery_Front;
-		$TTTGallery_Front = new TTTGallery_Front();
-		$TTTGallery_Front->init();
-	}
-	else {
-		global $TTTGallery_Admin;
-		$TTTGallery_Admin = new TTTGallery_Admin();
-		$TTTGallery_Admin->init();
-	}
+    $s = load_plugin_textdomain( 'tttgallery', false, dirname( plugin_basename( __FILE__ ) ) . '/lang/' );
+    if ( !is_admin() ) {
+        global $TTTGallery_Front;
+        $TTTGallery_Front = new TTTGallery_Front();
+        $TTTGallery_Front->init();
+    }
+    else {
+        global $TTTGallery_Admin;
+        $TTTGallery_Admin = new TTTGallery_Admin();
+        $TTTGallery_Admin->init();
+    }
 
 }
 
@@ -59,11 +59,11 @@ add_action('init', 'tttgallery_init');
 register_uninstall_hook( __FILE__ , 'tttgallery_uninstall' );
 
 function tttgallery_uninstall() {
-	require_once TTTINC_GALLERY . '/class/TTTGallery_Common.php';
-	require_once TTTINC_GALLERY . '/class/TTTGallery_Admin.php';
+    require_once TTTINC_GALLERY . '/class/TTTGallery_Common.php';
+    require_once TTTINC_GALLERY . '/class/TTTGallery_Admin.php';
 
-	$TTTGallery_Admin = new TTTGallery_Admin();
-	$TTTGallery_Admin->uninstall();
+    $TTTGallery_Admin = new TTTGallery_Admin();
+    $TTTGallery_Admin->uninstall();
 }
 
 
